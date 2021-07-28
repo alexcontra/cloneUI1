7af739eb8de2afefddf0436fb0060b26fff1fea7
Alex Contra <contraalex8@gmail.com>
2021-07-28 13:51:10 +0300

    Top part done

--- lib/functions/functions_file.dart
+++ lib/functions/functions_file.dart
@@ -1,1 +1,29 @@
 //generating widgets function will be here
+
+// creating stack function for slide menu .
+//creating stack function for every coffe page
+
+//create container for list view  under search text field
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Widget listContainer(Color colorofcontainer1, Color colorofcontainer2,
+    String text, Color textcolor) {
+  return Container(
+    width: 100.0,
+    height: 40.0,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20.0),
+      gradient: LinearGradient(colors: [
+        colorofcontainer2,
+        colorofcontainer1,
+      ]),
+    ),
+    child: Center(
+      child: Text(
+        '$text',
+        style: GoogleFonts.maitree(fontSize: 14.0, color: textcolor),
+      ),
+    ),
+  );
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,8 @@
+import 'package:clone_ui_1/functions/functions_file.dart';
+import 'package:clone_ui_1/functions/vars.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -12,14 +15,115 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
+        child: Stack(
           children: [
-            //caffe_cup5 and 7 are working fine
-            Image(image: AssetImage('assets/caffe_cup8.png')),
+            Positioned(
+              right: -10.0,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: 100.0,
+                color: Color.fromRGBO(160, 82, 45, 0.3),
+              ),
+            ),
+            Positioned(
+              top: 115.0,
+              child: Padding(
+                padding: EdgeInsets.only(left: 17.0),
+                child: Text('CoffeHouse',
+                    style: GoogleFonts.robotoCondensed(
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w700,
+                    )),
+              ),
+            ),
+            Positioned(
+              top: 148.0,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Text('Your goto place for the world\'s best coffe.',
+                    style: GoogleFonts.rochester(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w400,
+                    )),
+              ),
+            ),
+            Positioned(
+              top: 190.0,
+              left: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      brownModified,
+                      colorPinkModified,
+                    ]),
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0))),
+                width: 80.0,
+                height: 50.0,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 40.0),
+                  child: IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.search_outlined),
+                    onPressed: null,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 190.0,
+              child: Padding(
+                padding: EdgeInsets.only(left: 100.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 1.2,
+                  height: 50.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search for products',
+                      hintStyle: GoogleFonts.rochester(
+                        fontSize: 20.0,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 255.0,
+              child: Padding(
+                padding: EdgeInsets.only(left: 25.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 45.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      listContainer(colorPinkModified, brownModified, 'RITA',
+                          Colors.white),
+                      SizedBox(width: 10.0),
+                      listContainer(colorPinkModified, brownModified,
+                          'COLD BREW', Colors.black.withOpacity(0.6)),
+                      SizedBox(width: 10.0),
+                      listContainer(colorPinkModified, brownModified, 'NESCAFE',
+                          Colors.black.withOpacity(0.6)),
+                      SizedBox(width: 10.0),
+                      listContainer(colorPinkModified, brownModified,
+                          'ESSPRESO', Colors.black.withOpacity(0.6)),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
