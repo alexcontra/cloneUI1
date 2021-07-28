8ae5be0157fa6023017cbdc1cf46e56c02d8623e
Alex Contra <contraalex8@gmail.com>
2021-07-28 09:48:19 +0300

    prepared basics

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -10,6 +11,18 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            //caffe_cup5 and 7 are working fine
+            Image(image: AssetImage('assets/caffe_cup8.png')),
+          ],
+        ),
+      ),
+    );
   }
 }
